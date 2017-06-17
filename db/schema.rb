8309d5eae25b5cbada1f642670cc7e0a66794dba
witschia <witschia@163.com>
2017-06-17 14:59:40 +0800

    update version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20170617030155) do
+ActiveRecord::Schema.define(version: 20170617040504) do
 
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|

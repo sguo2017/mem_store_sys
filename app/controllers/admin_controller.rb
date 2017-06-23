f10b94932dd64de16199ad41a91d05354c8355f6
sguo2017 <sguo08@qq.com>
2017-06-23 14:18:27 +0800

    reprot memu

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,28 +1,38 @@
 class AdminController < ApplicationController
-	before_action :authenticate_user!
-	before_action :phone_access_admin
+    before_action :authenticate_user!
+    before_action :phone_access_admin    
+    before_action :menu_active
 
-  def phone_access_admin  	
+  def phone_access_admin    
     @user = current_user 
     #前端用户从前端跳转过来的 重新跳转到前端
     unless @user.admin 
-     	redirect_to [:phone, 'homepages'] 
+        redirect_to [:phone, 'homepages'] 
     end
+
+  end
+
+  def menu_active    
     @current_uri = request.fullpath
     @current_nav = @current_uri[/^\/admin\/([a-z_]+)[\?\/]{0,1}.?*/,1].to_s
     if @current_nav.blank?
-    	 puts "error! current_nav is blank!!!"
+         logger.debug "error! current_nav is blank!!!"
+    else
+        logger.debug "@current_nav #{@current_nav}"
     end
     @goods_menu = ["goods_catalogs"]
     @user_menu = ["lotteries", "score_histories", "mem_groups"]
     @bonus = ["bonus_changes", "mem_levels"]
+    @report = ["report"]
     if @goods_menu.include?(@current_nav) 
-    	@current_nav = "goods"
+        @current_nav = "goods"
     elsif @user_menu.include?(@current_nav)
-    	@current_nav = "users"
+        @current_nav = "users"
     elsif @bonus.include?(@current_nav)
-    	@current_nav = "bonus"
+        @current_nav = "bonus"
+    elsif @bonus.include?(@report)
+        @current_nav = "report"
     end
+  end
 
-	end
 end

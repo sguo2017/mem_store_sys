660990354b6ec1cad463161299c45db4c6ab6584
sguo2017 <sguo08@qq.com>
2017-06-30 10:18:53 +0800

    邀请好友URL地址要用域名来生成二维码

--- app/models/const.rb
+++ app/models/const.rb
@@ -1,6 +1,6 @@
 class Const
-  GOODS_SHOW_ADDR = "http://47.93.186.221:8081"
-  STORES_SHOW_ADDR = "http://47.93.186.221:8081"
+  GOODS_SHOW_ADDR = "http://gzb.davco.cn" #"http://47.93.186.221:8081"
+  STORES_SHOW_ADDR = "http://gzb.davco.cn" #"http://47.93.186.221:8081"
   ACTIVITY_SHOW_PAGE = {:base => "base", :award => "award", :cfg => "cfg"} 
   #初始化奖项
   AWARD_COUNT=10

92783b40ec7394e3acac3c663c423fe12c87089f
tanglongwen <tanglongwen@gmail.comgit config --global user.name tanglongwengit config --global user.email tanglongwen@gmail.com>
2017-07-27 17:17:30 +0800

    member region analyses

--- app/controllers/admin/report/region_analyses_controller.rb
+++ app/controllers/admin/report/region_analyses_controller.rb
@@ -1,5 +1,7 @@
 class Admin::Report::RegionAnalysesController < AdminController
   def index
-   
+	@data = User.select("province,count(1) as count")
+				.group(:province)
+				.order("count DESC")
   end
 end

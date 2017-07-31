70d94a203a7b690d1cda1868e3feaf17f3fb5610
tanglongwen <tanglongwen@gmail.comgit config --global user.name tanglongwengit config --global user.email tanglongwen@gmail.com>
2017-07-31 09:42:05 +0800

    style modify

--- app/controllers/admin/report/mem_analyses_controller.rb
+++ app/controllers/admin/report/mem_analyses_controller.rb
@@ -13,7 +13,7 @@ class Admin::Report::MemAnalysesController < AdminController
         middle_age=0
         old=0
         other=0
-        users=User.where([ "id > ?  ",  1 ])
+        users=User.where([ "admin = ?  ", false ])
         users.each do |t|
             unless t.birthday.nil?
                 age = (DateTime.now - t.birthday) / 365.25
@@ -33,31 +33,32 @@ class Admin::Report::MemAnalysesController < AdminController
             {value:middle_age, name:'中年'},
             {value:old, name:'老年'},
             {value:other, name:'其它(未知年龄)'}
-          ]
-        # logger.debug("未成年人数: #{@minors}")     
+          ]  
     when "2" #性别  
         #统计性别人数
-        men=User.where([ "id > ? and sex = ? ", 1 , 1 ])
+        men=User.where([ "admin = ? and sex = ? ", false , 1 ])
         men_num=men.length
         
-        women=User.where([ "id > ? and sex = ? ", 1 , 0 ])
+        women=User.where([ "admin = ? and sex = ? ", false , 0 ])
         women_num=women.length
         @data = [
             {value:men_num, name:'男性'},
             {value:women_num, name:'女性'},
           ]
+
+       
     when "3" 
          #统计等级人数
-        level_v1=User.where([ "id > ? and level = ? ", 1 , 'V1' ])
+        level_v1=User.where([ "admin = ? and level = ? ", false , 'V1' ])
         level_v1_num=level_v1.length
         
-        level_v2=User.where([ "id > ? and level = ? ", 1 , 'V2' ])
+        level_v2=User.where([ "admin = ? and level = ? ", false , 'V2' ])
         level_v2_num=level_v2.length
 
-        level_v3=User.where([ "id > ? and level = ? ", 1 , 'V3' ])
+        level_v3=User.where([ "admin = ? and level = ? ", false , 'V3' ])
         level_v3_num=level_v3.length
 
-        level_v4=User.where([ "id > ? and level = ? ", 1 , 'V4' ])
+        level_v4=User.where([ "admin = ? and level = ? ", false , 'V4' ])
         level_v4_num=level_v4.length
 
          @data = [
@@ -66,15 +67,6 @@ class Admin::Report::MemAnalysesController < AdminController
             {value:level_v3_num, name:'白金会员'},
             {value:level_v4_num, name:'钻石会员'}
           ]
-            
     end
-    # @data=
-    # 	[
-    #         {value:335, name:'直接访问'},
-    #         {value:310, name:'邮件营销'},
-    #         {value:234, name:'联盟广告'},
-    #         {value:135, name:'视频广告'},
-    #         {value:1548, name:'搜索引擎'}
-    #       ]
   end
 end

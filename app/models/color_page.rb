ec1cbfd5c33dfbdc6b7903431639757bd27808ab
chenhaihong <chh4441@qq.com>
2017-09-19 16:29:50 +0800

    1.A 活动配置页面新增：文件选择、选择用户、日期选择；2.A 模型添加注释。

--- app/models/color_page.rb
+++ app/models/color_page.rb
@@ -1,3 +1,9 @@
+# name               活动名称
+# begin_time         开始时间
+# end_time           结束时间
+# profile            活动主题
+# avatar             活动图片
+# content            活动内容
+# accept_users_type  0 => 不发送全部用户, 1 => 发送全部用户
 class ColorPage < ApplicationRecord
-	mount_uploader :avatar, AvatarUploader
 end

46309d4ce6a9f84086c649c650cb8b562509e3f1
sguo2017 <sguo08@qq.com>
2017-09-22 15:55:33 +0800

    会员积分体系->德叔之家

--- app/models/const.rb
+++ app/models/const.rb
@@ -2,6 +2,7 @@ class Const
   GOODS_SHOW_ADDR = "http://gzb.davco.cn" #"http://47.93.186.221:8081"
   STORES_SHOW_ADDR = "http://gzb.davco.cn" #"http://47.93.186.221:8081"
   DOMAIN = "http://gzb.davco.cn"
+  WEB_NAME = "德叔之家"
   ACTIVITY_SHOW_PAGE = {:base => "base", :award => "award", :cfg => "cfg"} 
   #初始化奖项
   AWARD_COUNT=10

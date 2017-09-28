15518137241917faeb7c9af832486e1ddd4e0f03
chenhaihong <chh4441@qq.com>
2017-09-28 11:39:01 +0800

    1.A 门店页用户列表模态框添加条件筛选

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -6,13 +6,13 @@ class Admin::StoresController < AdminController
   def index
     @name = params[:name]
     if current_user.admin == 1
-      if @name.blank? 
+      if @name.blank?
         @stores = Store.page(params[:page]).per(10)
       else
         @stores = Store.where('name LIKE ? ', '%'+@name+'%').order("created_at DESC").page(params[:page]).per(10)
       end
     else
-      if @name.blank? 
+      if @name.blank?
         @stores = current_user.managestores.page(params[:page]).per(10)
       else
         @stores = current_user.managestores.where('name LIKE ? ', '%'+@name+'%').order("created_at DESC").page(params[:page]).per(10)
@@ -22,15 +22,15 @@ class Admin::StoresController < AdminController
 
   # GET /admin/stores/1
   # GET /admin/stores/1.json
-  def show 
+  def show
     if current_user.admin == 2
       unless current_user.managestores.exists?(id: params[:id])
-         redirect_to main_app.root_url
-       end
+        redirect_to main_app.root_url
+      end
     end
     @store=Store.find(params[:id])
     if @store.qrcode.blank?
-	  info = ConfigInfo["weixinconfiginfo"]
+      info = ConfigInfo["weixinconfiginfo"]
       go_url = "#{info["AUTH_ADDR"]}appid=#{info["APPID"]}&redirect_uri=#{Const::STORES_SHOW_ADDR}/phone/mem_activations?store_id=#{@store.id.to_s}&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect"
       short_url_object = ShortUrl.new
       short_url_object.value = go_url
@@ -38,8 +38,8 @@ class Admin::StoresController < AdminController
       short_url = Const::GOODS_SHOW_ADDR + '/s/' + ShortUrl.decb64(short_url_object.id)
       p short_url
       qr = RQRCode::QRCode.new(short_url, :size => 4, :level => :h )
-      png = qr.to_img     
-      @store.qrcode = "/uploads/store/mem_activation/qrcode_"+@store.id.to_s+".png";                 
+      png = qr.to_img
+      @store.qrcode = "/uploads/store/mem_activation/qrcode_"+@store.id.to_s+".png";
       png.resize(172, 172).save(Rails.root.to_s + "/public/" + @store.qrcode)
       @store.save
     end
@@ -112,19 +112,32 @@ class Admin::StoresController < AdminController
     @name  = params[:name]
     @phone = params[:phone]
 
-    render json:{
-      user: [{'name': 'terry'}]
-    }
-  end
-  
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_store
-      @store = Store.find(params[:id])
+    _where = "1 = 1"
+    unless @name.blank?
+      _where = _where + " and name like '%#{@name}%'"
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def store_params
-      params.require(:store).permit(:code, :catalog, :name, :district, :city, :province, :country, :latitude, :longitude, :addr, :linkman, :contact_num)
+    unless @phone.blank?
+      _where = _where + " and phone_num like '%#{@phone}%'"
     end
+
+    @user = User.select("id, name, phone_num").where(_where).order('id DESC')#.page(params[:page]).per(10)
+
+    render json:{
+      success: true,
+      #sum: User.select("id").where(_where).count,
+      data: @user
+    }
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_store
+    @store = Store.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def store_params
+    params.require(:store).permit(:code, :catalog, :name, :district, :city, :province, :country, :latitude, :longitude, :addr, :linkman, :contact_num)
+  end
 end
